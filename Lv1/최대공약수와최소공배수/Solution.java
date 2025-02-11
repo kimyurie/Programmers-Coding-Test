3e7f9e72b990c38bc95e0769be5dc7ef5ce49a70
유리 <9911dbfl@naver.com>
2025-02-11 13:12:08 +0900

    [Refactor]: rv_최대공약수와 최소공배수

--- Lv1/최대공약수와최소공배수/Solution.java
+++ Lv1/최대공약수와최소공배수/Solution.java
@@ -1,5 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12940?language=java
 package Lv1.최대공약수와최소공배수;
+// review : 1 /
 
 class Solution {
     public int[] solution(int n, int m) {
@@ -8,7 +9,7 @@ class Solution {
         // 최대공약수 계산 (유클리드 호제법)
         int gcd = gcd(n, m);
 
-        // 최소공배수 계산
+        // 최소공배수 구하기 => (n * m) / 최대공약수
         int lcm = (n * m) / gcd;
 
         answer[0] = gcd; // 최대공약수
@@ -18,6 +19,13 @@ class Solution {
     }
 
     // 유클리드 호제법 사용해 최대공약수 계산
+    // ex) 106과 16의 최대공약수 구하기
+    //  106 / 16 = 6 ... 10
+    //        16 / 10 = 1 ... 6
+    //             10 / 6 = 1 ... 4
+    //                  6 / 4 = 1 ... 2
+    //                      4 / 2 = 2 ... 0
+    //            나머지가 0이되므로 나눠지는 수인 2가 최대공약수!
     // => 두 수 a,b의 최대공약수는 b와 a%b의 최대공약수와 동일
     // (나머지를 나누는 수에 계속 나누면 마지막 나누는 수가 최대공약수)
     private int gcd(int a, int b) {
